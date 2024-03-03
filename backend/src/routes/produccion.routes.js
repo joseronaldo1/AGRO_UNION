2f491f123dda45281142dc2e422810b99a53be02
pabloandre4112 <perdomo4112@gmail.com>
2024-03-02 22:25:39 -0500

    crud api con documentacion

---
+++ backend/src/routes/produccion.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { ValidateProduccion,actualizar } from "../../validate/Produccion.js";
+import { BuscarPoduccion, actualizarProduccion, listarProduccion, registrarProduccion } from "../controllers/Produccion.js";
+
+const produccion = Router()
+
+produccion.get('/listarProduccion',listarProduccion)
+produccion.post('/RegistraProduccion',ValidateProduccion,registrarProduccion)
+produccion.get('/BuscarProduccion',BuscarPoduccion)
+produccion.put('/ActualizarProduccion',actualizar,actualizarProduccion)
+
+
+export {produccion}
