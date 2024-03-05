f230f4b38804978ed1ec1efba70d80bb653fb5d9
josevargas31 <dajzoavargas@gmail.com>
2024-03-04 22:08:01 -0500

    changes index y conexion

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,18 @@
-console.log("tin...");
+import express from'express'
+import  body_parsere from 'body-parser'
+
+const servidor = express()
+
+servidor.use(body_parsere.json())
+servidor.use(body_parsere.urlencoded({extended: false}))
+
+servidor.set('view engine', 'ejs');
+servidor.set('views','./views');
+
+servidor.get('/document',(req,res)=>{
+    res.render('document.ejs');
+})
+
+servidor.listen(3000, () =>{
+    console.log("esta funcionando")
+})
