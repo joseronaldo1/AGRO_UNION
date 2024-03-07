88f55fbcd0ab8e3fc9e34bddbdc1fe1cca24c569
estebangaviria2006 <munozleandro343@gmail.com>
2024-03-06 19:10:06 -0500

    importacion token

--- backend/src/routes/variedad.routes.js
+++ backend/src/routes/variedad.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { registrarVariedad, listarVariedades, actualizarVariedad, desactivarVariedad, buscarVariedad } from "../controllers/variedad.controller.js";
 import { validarRegistroVariedad, validarActualizacionVariedad } from "../validate/validarVariedad.js";
+import {validarToken} from "../controllers/variedad.controller.js"
 
 const rutaDeVariedad = Router();
 
 rutaDeVariedad.post("/registrarVariedad", validarRegistroVariedad, registrarVariedad);
-rutaDeVariedad.get("/listarVariedades", listarVariedades);
+rutaDeVariedad.get("/listarVariedades",validarToken, listarVariedades);
 rutaDeVariedad.put("/actualizarVariedad/:id", validarActualizacionVariedad, actualizarVariedad);
 //rutaDeVariedad.put("/desactivarVariedad/:id", desactivarVariedad);
 rutaDeVariedad.get("/buscarVariedad/:id", buscarVariedad);
