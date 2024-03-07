81bff0ae6ee27ac52f1bca02fa7de87770184965
joseronaldo1 <ledezmaparrajoseronaldo@gmail.com>
2024-03-07 13:43:33 -0500

    merge funcional

---
+++ src/routes/produccion.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { ValidateProduccion,actualizar } from "../../validate/Produccion.js";
+import { BuscarProduccion, actualizarProduccion, listarProduccion, registrarProduccion } from "../controllers/Produccion.js";
+
+
+const produccion = Router()
+
+produccion.get('/listarProduccion',listarProduccion);
+produccion.post('/RegistraProduccion',ValidateProduccion,registrarProduccion);
+produccion.get('/BuscarProduccion',BuscarProduccion);
+produccion.put('/ActualizarProduccion/id_produccion',actualizar,actualizarProduccion);
+
+export default produccion
