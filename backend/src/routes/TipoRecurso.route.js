940f077254a8a75b0d182064af112688c8bcc08c
bravodaniel12 <danigobra2020@gmail.com>
2024-03-06 22:32:54 -0500

    falta token de karen

--- backend/src/routes/TipoRecurso.route.js
+++ backend/src/routes/TipoRecurso.route.js
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import { listarTipoRecurso, RegistroTipoRecurso, ActualizarTipoRecurso, BuscarTipoRecurso 
 } from "../controllers/TipoRecurso.controller.js";
-import { validarTra } from "../validate/tipo_recurso.js";
-import { validarTrr } from "../validate/tipo_recurso.js";
+import { validarTra } from "../../validate/tipo_recurso.js";
+import { validarTrr } from "../../validate/tipo_recurso.js";
+
+import {ValidarToken} from "../controllers/autenticacion.js";
 const rutaDeTipoRecurso = Router()
 
 //localhost:4000/VariedadCultivo
-rutaDeTipoRecurso.get("/listarRecurso", listarTipoRecurso);
-rutaDeTipoRecurso.post("/RegistroRecurso",validarTrr, RegistroTipoRecurso);
-rutaDeTipoRecurso.put("/actualizarRecurso/:id",validarTra, ActualizarTipoRecurso);
+rutaDeTipoRecurso.get("/listarRecurso", ValidarToken, listarTipoRecurso);
+rutaDeTipoRecurso.post("/RegistroRecurso", ValidarToken, validarTrr, RegistroTipoRecurso);
+rutaDeTipoRecurso.put("/actualizarRecurso/:id",ValidarToken ,validarTra, ActualizarTipoRecurso);
 /* rutaDeTipoRecurso.put("/desactivar/Recurso/:id", DesactivarTipoRecurso); */
-rutaDeTipoRecurso.get("/buscarRecurso/:id", BuscarTipoRecurso);
+rutaDeTipoRecurso.get("/buscarRecurso/:id", ValidarToken, BuscarTipoRecurso);
 
 export { rutaDeTipoRecurso };
 
