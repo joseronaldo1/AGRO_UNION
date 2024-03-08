a3f6c22c1bc31fd17801218bb364881220a5f298
karenviviana1234 <karenvivianadiazguevara@gmail.com>
2024-03-08 09:57:29 -0500

    conflicto resuelto

--- backend/src/routes/routes.Recursos.js
+++ backend/src/routes/routes.Recursos.js
@@ -1,12 +1,7 @@
 import { Router } from "express";
-<<<<<<< HEAD:src/routes/routes.Recursos.js
 import {registrar,  actualizar,  buscar, listar } from '../controller/controllerRecursos.js';
 import { validacionRecursosR } from "../../validate/validacionRecursos.js";
 import { validacionRecursosA } from "../../validate/validacionRecursos.js";
-=======
-import {registrar,  actualizar,  buscar, listar } from '../controllers/controllerRecursos.js';
-import {check} from 'express-validator';
->>>>>>> e366ffcc4c20baf1711bee3b9fe84c0990dfcad5:backend/src/routes/routes.Recursos.js
 
 
 const rutaRecursos = Router();
