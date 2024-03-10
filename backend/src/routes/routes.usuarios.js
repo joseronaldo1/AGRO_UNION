99fe1c4dea268aaf17091f537044be5169446625
karenviviana1234 <karenvivianadiazguevara@gmail.com>
2024-03-10 14:35:11 -0500

    cambios

--- backend/src/routes/routes.usuarios.js
+++ backend/src/routes/routes.usuarios.js
@@ -7,7 +7,7 @@ import { validarToken } from '../controllers/autenticacion.js'
 const rutaUsuario = Router();
 
 rutaUsuario.get('/listarUsuario', validarToken,listarUsuarios);  
-rutaUsuario.post('/registrarUsuario', validarToken,validarUsuario,registrarUsuarios);
+rutaUsuario.post('/registrarUsuario',validarUsuario,registrarUsuarios);
 rutaUsuario.post('/desactivarUsuario/:id_usuario',validarToken, desactivarUsuario);
 rutaUsuario.put('/actualizarUsuario/:id_usuario',validarToken, validarUsu,actualizarUsuario);
 rutaUsuario.get('/buscarUsuario/:id_usuario',validarToken, buscarUsuario);
