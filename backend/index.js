fe882c4eb451054b18594905834df0f8e697785f
joseronaldo1 <ledezmaparrajoseronaldo@gmail.com>
2024-03-11 00:44:24 -0500

    cambios devsdv

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 import express  from 'express' 
 import  body_parser from 'body-parser'
-import routeUsuarios from './src/routes/routes.usuarios.js'
-import rutaValidacion from './src/routes/route.autenticacion.js'
-import costos from './src/routes/costos.routes.js';
-import rutaFinca from './src/routes/Finca.routes.js';
-import rutalote from './src/routes/lotes.routes.js';
+//import routeUsuarios from './src/routes/routes.usuarios.js'
+//import rutaValidacion from './src/routes/route.autenticacion.js'
+//import costos from './src/routes/costos.routes.js';
+//import rutaFinca from './src/routes/Finca.routes.js';
+//import rutalote from './src/routes/lotes.routes.js';
 
-import rutaAsignacion from './src/routes/routes.asignacion.js';
+//import rutaAsignacion from './src/routes/routes.asignacion.js';
 import rutaRecursos from './src/routes/routes.Recursos.js';
-import { rutaDeTipoRecurso } from './src/routes/TipoRecurso.route.js';
+//import { rutaDeTipoRecurso } from './src/routes/TipoRecurso.route.js';
 
 
 const servidor = express()
@@ -24,14 +24,14 @@ servidor.get('/document',(req,res)=>{
     res.render('document.ejs');
 })
 
-servidor.use('/usuarios', routeUsuarios)
-servidor.use(rutaValidacion)
-servidor.use(costos)
-servidor.use(rutaFinca)
-servidor.use(rutalote)
-servidor.use(rutaAsignacion)
+//servidor.use('/usuarios', routeUsuarios)
+//servidor.use(rutaValidacion)
+//servidor.use(costos)
+//servidor.use(rutaFinca)
+//servidor.use(rutalote)
+//servidor.use(rutaAsignacion)
 servidor.use(rutaRecursos)
-servidor.use(rutaDeTipoRecurso)
+//servidor.use(rutaDeTipoRecurso)
 
 servidor.listen(3000, () =>{
     console.log("esta funcionando")
