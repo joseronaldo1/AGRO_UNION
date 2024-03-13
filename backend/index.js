5830c8b2cd8b3b68fd0d4ab07434cd08b3a47be1
bravodaniel12 <vanesacha06@gmail.com>
2024-03-13 15:00:17 -0500

    akls

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,11 @@
 import express  from 'express' 
 import  body_parser from 'body-parser'
-import { rutaDeTipoRecurso } from './src/routes/TipoRecurso.route.js';
-import { rutaDeActividad } from './src/routes/Actividad.route.js';
-
 
 const servidor = express()
 
 servidor.use(body_parser.json())
 servidor.use(body_parser.urlencoded({extended: false}))
 
-
-servidor.set('view engine', 'ejs');
-servidor.set('views','./views');
-
-servidor.get('/document',(req,res)=>{
-    res.render('documentdevdfg.ejs');
-})
-
-servidor.use(express.static('./public'));
-
-servidor.use(rutaDeTipoRecurso)
-servidor.use(rutaDeActividad)
 
 servidor.listen(3000, () =>{
     console.log("esta funcionando")
